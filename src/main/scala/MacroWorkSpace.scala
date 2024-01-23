114af46897546cb52c150c9992376cc58b122c46
Adrian Garcia <adrian.garcia@estudiants.urv.cat>
2024-01-23 15:21:54 +0100

    test + read from file added

--- src/main/scala/MacroWorkSpace.scala
+++ src/main/scala/MacroWorkSpace.scala
@@ -1,15 +1,60 @@
 package main
+import scala.io.Source
+import java.io.FileNotFoundException
+import java.io.IOException
 
 object MacroWorkSpace extends App {
-  // WordCount function (counts the number of occurrences of each word in a list of texts)
-  def wordCount(texts: List[String]): Map[String, Int] = {
-    texts
-      .flatMap(_.toLowerCase.split("\\W+"))
-      .groupMapReduce(identity)(_ => 1)(_ + _)
+  /**
+   * Counts the number of occurrences of each word in a list of texts or a single text.
+   *
+   * @param texts The list of texts or a single text to analyze.
+   * @return A map where each word is a key, and its value is the number of occurrences.
+   */
+  def wordCount(texts: Either[List[String], String]): Map[String, Int] = {
+    texts match {
+      case Left(list) =>
+        list
+          .flatMap(_.toLowerCase.split("\\W+"))
+          .groupMapReduce(identity)(_ => 1)(_ + _)
+      case Right(text) =>
+        text
+          .toLowerCase.split("\\W+")
+          .groupMapReduce(identity)(_ => 1)(_ + _)
+    }
   }
 
-  // CountWords function (counts the total number of words in a list of texts)
-  def countWords(texts: List[String]): Int = {
-    texts.flatMap(_.split("\\W+")).length
+  /**
+   * Counts the total number of words in a list of texts or a single text.
+   *
+   * @param texts The list of texts or a single text to analyze.
+   * @return The total number of words in the list or text.
+   */
+  def countWords(texts: Either[List[String], String]): Int = {
+    texts match {
+      case Left(list) => list.flatMap(_.split("\\W+")).length
+      case Right(text) => text.split("\\W+").length
+    }
+  }
+
+/**
+   * Reads the content of the specified file and returns it as a string.
+   *
+   * @param filePath The path to the file to be read.
+   * @return The content of the file as a string.
+   */
+  def readText(filePath: String): String = {
+    try {
+      val source = Source.fromFile(filePath, "UTF-8")
+      try {
+        source.mkString
+      } finally {
+        source.close()
+      }
+    } catch {
+      case e: FileNotFoundException =>
+        s"Error: File not found - $filePath"
+      case e: IOException =>
+        s"Error reading file - $e.getMessage"
+    }
   }
 }
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -4,19 +4,20 @@ import main.MacroWorkSpace
 import munit.FunSuite
 
 class Tests extends FunSuite {
-  // List of strings to be read
-  val sampleTexts: List[String] = List(
-    "This is an example text.",
-    "It contains some words,",
-    "and we want to count the occurrences of each word.",
-    "is to, of"
-  )
-
-  // Get word count and total word count
-  val wordCount: Map[String, Int] = MacroWorkSpace.wordCount(sampleTexts)
-  val countWords: Int = MacroWorkSpace.countWords(sampleTexts)
 
   test("word count test") {
+    // List of strings to be read
+    val sampleTexts: List[String] = List(
+      "This is an example text.",
+      "It contains some words,",
+      "and we want to count the occurrences of each word.",
+      "is to, of"
+    )
+
+    // Get word count and total word count
+    val wordCount: Map[String, Int] = MacroWorkSpace.wordCount(Left(sampleTexts))
+    val countWords: Int = MacroWorkSpace.countWords(Left(sampleTexts))
+
     // Assert that the word count for specific words is as expected
     assertEquals(wordCount.getOrElse("this", 1), 1)
     assertEquals(wordCount.getOrElse("example", 1), 1)
@@ -25,5 +26,54 @@ class Tests extends FunSuite {
 
     // Assert that the total word count is as expected
     assertEquals(countWords, 22)
+  }
+
+  test("read text test") {
+    // Create a temporary file with some content for testing
+    val filePath = "file.txt"
+
+    val fileContent = MacroWorkSpace.readText(filePath)
+    println(s"File Content:\n$fileContent")
+
+    val wordCount: Map[String, Int] = MacroWorkSpace.wordCount(Right(fileContent))
+    val countWords: Int = MacroWorkSpace.countWords(Right(fileContent))
+
+    // Assert that the word count for specific words is as expected
+    assertEquals(wordCount.getOrElse("this", 1), 1)
+    assertEquals(wordCount.getOrElse("example", 1), 1)
+    assertEquals(wordCount.getOrElse("to", 2), 2)
+    assertEquals(wordCount.getOrElse("of", 2), 2)
+
+    // Assert that the total word count is as expected
+   assertEquals(countWords, 22)
+  }
+
+    test("read temporal") {
+    // Create a temporary file with some content for testing
+    val tempFilePath = "temporal.txt"
+    val tempFileContent = "This is a test one this is a test."
+
+    // Write content to the temporary file
+    java.nio.file.Files.write(
+      java.nio.file.Paths.get(tempFilePath),
+      tempFileContent.getBytes(java.nio.charset.StandardCharsets.UTF_8)
+    )
+
+    // Read content from the temporary file using readText function
+    val fileContent = MacroWorkSpace.readText(tempFilePath)
+    println(s"File Content:\n$fileContent")
+
+    val wordCount: Map[String, Int] = MacroWorkSpace.wordCount(Right(fileContent))
+    val countWords: Int = MacroWorkSpace.countWords(Right(fileContent))
+    // Assert that the content read from the file is the same as expected
+    assertEquals(fileContent, tempFileContent)    
+    assertEquals(wordCount.getOrElse("this", 2), 2)
+    assertEquals(wordCount.getOrElse("is", 2), 2)
+    assertEquals(wordCount.getOrElse("a", 2), 2)
+    assertEquals(wordCount.getOrElse("test", 2), 2)
+    assertEquals(wordCount.getOrElse("one", 1), 1)
+    
+    // Assert that the total word count is as expected
+    assertEquals(countWords, 9)
   }
 }
