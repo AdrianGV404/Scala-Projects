555a9fac69f2b5500869441f0b1a888508880314
Adrian Garcia <adrian.garcia@estudiants.urv.cat>
2024-01-23 15:47:33 +0100

    singleton

--- src/main/scala/Singleton.scala
+++ src/main/scala/Singleton.scala
@@ -1,10 +1,19 @@
 package main
 
+/**
+ * Singleton object representing a controller with an ID.
+ * This object is designed as a singleton, ensuring that there is only one instance of the controller in the application.
+ */
 object Singleton extends App {
   // Controller ID
   private var _id: String = "SingletonId"
 
-  // Function to get the current ID
+  /**
+   * Gets the current ID of the Singleton.
+   * If the ID is not initialized, it is set to the default value "SingletonId" before being returned.
+   *
+   * @return The current ID of the Singleton.
+   */
   def getId: String = {
     if (_id == null) {
       _id = "SingletonId"
@@ -12,7 +21,13 @@ object Singleton extends App {
     _id
   }
 
-  // Constructor that allows setting the ID
+  /**
+   * Creates a new instance of the Singleton with a custom ID.
+   * This method allows setting a custom ID for the Singleton instance.
+   *
+   * @param newId The custom ID to set for the Singleton.
+   * @return The Singleton instance with the custom ID.
+   */
   def create(newId: String): Singleton.type = {
     _id = newId
     this
