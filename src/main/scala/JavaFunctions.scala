740431b6e6455c6cb70cf72b6c0509f1df2e61f7
Adrian Garcia <adrian.garcia@estudiants.urv.cat>
2024-01-24 12:11:03 +0100

    Scala exceutes java code OK + mini test, correct tests are missing

--- src/main/scala/JavaFunctions.scala
+++ src/main/scala/JavaFunctions.scala
@@ -1,22 +1,29 @@
 package main.scala
 
-import main.java.{Controller, Invoker, Action, Metric, reflection, policy, operations, mapReduce, interfaces, exceptions, decorator}
+import main.java.{Controller, Invoker, Action, Metric}
+import main.java.reflection.{ActionProxy,DynamicProxy}
+import main.java.policy.{UniformGroup, RoundRobinImproved, RoundRobin, GreedyGroup, BigGroup}
+import main.java.operations.{Adder, CountWords, Factorial, Multiplier, WordCount}
+import main.java.mapReduce.{Reduce, TextReader}
+import main.java.interfaces.{DistributionPolicy, InterfaceAction, InterfaceInvoker, Observer}
+import main.java.exceptions.InsufficientMemoryException
+import main.java.decorator.{ActionResult, InvokerCacheDecorator, InvokerChronometerDecorator}
 
 object JavaFunctions {
-  def main(args: Array[String]): Unit = {
-    // Crear una instancia del controlador
+  def main(args: Array[String]): Unit = {    
     val controller: Controller = Controller.getInstance()
-
-    // Hacer llamadas a métodos de la instancia del controlador
+    controller.setPolicy(new GreedyGroup());
     val controllerId: Int = controller.getId()
     println(s"Controller ID: $controllerId")
-    
-    // Agregar invocadores, acciones, etc. según sea necesario
-    val invoker = new Invoker(1024, "Invoker1") // Ejemplo, ajusta según tu lógica
+
+    val invoker = new Invoker(1024, "Invoker1") 
     controller.addInvoker(invoker)
 
-    val action = new Action("Action1", 512) // Ejemplo, ajusta según tu lógica
-    controller.addAction(action)
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    val values: Array[Int] = list.toArray //"casting" to avoid type problem
+    val add1: Adder = new Adder("add1", 2000, {values});
+        
+    controller.addAction(add1)
 
     // Distribuir acciones (puedes personalizar esto según tu lógica)
     controller.distributeActions()
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -3,6 +3,14 @@ package test.scala
 import main.scala.ScalaFunctions
 import main.scala.Singleton
 import munit.FunSuite
+import main.java.{Controller, Invoker, Action, Metric}
+import main.java.reflection.{ActionProxy,DynamicProxy}
+import main.java.policy.{UniformGroup, RoundRobinImproved, RoundRobin, GreedyGroup, BigGroup}
+import main.java.operations.{Adder, CountWords, Factorial, Multiplier, WordCount}
+import main.java.mapReduce.{Reduce, TextReader}
+import main.java.interfaces.{DistributionPolicy, InterfaceAction, InterfaceInvoker, Observer}
+import main.java.exceptions.InsufficientMemoryException
+import main.java.decorator.{ActionResult, InvokerCacheDecorator, InvokerChronometerDecorator}
 
 class Tests extends FunSuite {
 
@@ -93,6 +101,29 @@ class Tests extends FunSuite {
   }
 
     test("Java Functions Working") {
-      
+      def main(args: Array[String]): Unit = {    
+    val controller: Controller = Controller.getInstance()
+    controller.setPolicy(new GreedyGroup());
+    val controllerId: Int = controller.getId()
+    println(s"Controller ID: $controllerId")
+
+    val invoker = new Invoker(1024, "Invoker1") 
+    controller.addInvoker(invoker)
+
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    val values: Array[Int] = list.toArray //"casting" to avoid type problem
+    val add1: Adder = new Adder("add1", 2000, {values});
+        
+    controller.addAction(add1)
+
+    // Distribuir acciones (puedes personalizar esto según tu lógica)
+    controller.distributeActions()
+
+    // Ejecutar acciones asignadas
+    controller.executeAssignedActions()
+
+    // Imprimir métricas
+    controller.printMetrics()
+  }
   }
 }
